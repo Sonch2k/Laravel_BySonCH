71df31d55618d1a5169cf7762ffb0b7a214bccca
DESKTOP-IGORRM2\son tuan <sonchhe2k@gmail.com>
2021-10-31 22:41:06 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('loginForm', function () {
-    view('results')->with('message', 'Thanh Cong');
+Route::post('loginForm', function () {
+    return view('login');
 });
 
